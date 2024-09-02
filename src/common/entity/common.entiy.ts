f3a5df8994c0a73cf39156d273daa3367f029f0f
FourteenDP <fourteendp@qq.com>
2024-09-02 19:45:35 +0800

    通用实体

--- src/common/entity/common.entiy.ts
+++ src/common/entity/common.entiy.ts
@@ -1,51 +1,66 @@
-import e from 'express';
+import { ApiHideProperty, ApiProperty } from '@nestjs/swagger';
+import { Exclude } from 'class-transformer';
 import {
-  PrimaryGeneratedColumn,
-  CreateDateColumn,
-  UpdateDateColumn,
-  Column,
   BaseEntity,
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  VirtualColumn,
 } from 'typeorm';
 
+// 如果觉得前端转换时间太麻烦，并且不考虑通用性的话，可以在服务端进行转换，eg: @UpdateDateColumn({ name: 'updated_at', transformer })
+// const transformer: ValueTransformer = {
+//   to(value) {
+//     return value
+//   },
+//   from(value) {
+//     return dayjs(value).format('YYYY-MM-DD HH:mm:ss')
+//   },
+// }
+
 export abstract class CommonEntity extends BaseEntity {
-  @PrimaryGeneratedColumn({
-    type: 'int',
-    name: 'id',
-    comment: '主键ID',
-  })
+  @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({
-    type: 'tinyint',
-    name: 'is_delete',
-    comment: '是否删除',
-    default: 0,
-  })
-  isDelete: number;
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
 
-  @CreateDateColumn({
-    type: 'timestamp',
-    name: 'create_time',
-    comment: '创建时间',
-  })
-  createTime: Date;
-
-  @UpdateDateColumn({
-    type: 'timestamp',
-    name: 'update_time',
-    comment: '更新时间',
-  })
-  updateTime: Date;
-
-  @Column({
-    type: 'timestamp',
-    name: 'delete_time',
-    comment: '删除时间',
-    nullable: true,
-  })
-  deleteTime: Date;
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
 }
 
-export abstract class CompleteEntity extends BaseEntity {
+export abstract class CompleteEntity extends CommonEntity {
+  @ApiHideProperty()
+  @Exclude()
+  @Column({
+    name: 'create_by',
+    update: false,
+    comment: '创建者',
+    nullable: true,
+  })
+  createBy: number;
 
+  @ApiHideProperty()
+  @Exclude()
+  @Column({ name: 'update_by', comment: '更新者', nullable: true })
+  updateBy: number;
+
+  /**
+   * 不会保存到数据库中的虚拟列，数据量大时可能会有性能问题，有性能要求请考虑在 service 层手动实现
+   * @see https://typeorm.io/decorator-reference#virtualcolumn
+   */
+  @ApiProperty({ description: '创建者' })
+  @VirtualColumn({
+    query: (alias) =>
+      `SELECT username FROM sys_user WHERE id = ${alias}.create_by`,
+  })
+  creator: string;
+
+  @ApiProperty({ description: '更新者' })
+  @VirtualColumn({
+    query: (alias) =>
+      `SELECT username FROM sys_user WHERE id = ${alias}.update_by`,
+  })
+  updater: string;
 }
